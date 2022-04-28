74feeee35aab88944eabb2f970962103965c2a1d
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-04-28 08:32:02 -0400

    feat: funciones utiles para slices

--- hola-mundo.go
+++ hola-mundo.go
@@ -13,12 +13,14 @@ func main() {
 		"El Rey Escorpión",
 		"Paprika",
 		"Piola"}
-
 	// Añade elementos al slice
 	peliculas = append(peliculas, "Batman")
-
 	fmt.Print("Todas las pelis: ")
 	fmt.Println(peliculas)
+	fmt.Print("Cantidad de pelis: ")
+	fmt.Println(len(peliculas))
+	fmt.Print("Pelis del 0 al 3: ")
+	fmt.Println(peliculas[0:3])
 }
 
 func holaGo() {
