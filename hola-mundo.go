16000dd0bb6d7de8401390cc1e4f448caa9f581c
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-04-28 09:25:39 -0400

    feat: leer desde consola y condiciones para edad

--- hola-mundo.go
+++ hola-mundo.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	holaGo()
-	peliculas := []string{
-		"El Retorno del Rey",
-		"Aladdin",
-		"Nemo",
-		"El Rey Escorpión",
-		"Paprika",
-		"Piola"}
-	// Añade elementos al slice
-	peliculas = append(peliculas, "Batman")
-	fmt.Print("Todas las pelis: ")
-	fmt.Println(peliculas)
-	fmt.Print("Cantidad de pelis: ")
-	fmt.Println(len(peliculas))
-	fmt.Print("Pelis del 0 al 3: ")
-	fmt.Println(peliculas[0:3])
+	//Toma valores que paso por defecto al ejecutar app
+	fmt.Println("Hola " + os.Args[1] + "!")
+	convertValue, err := strconv.Atoi(os.Args[2])
+	if err == nil {
+		fmt.Print("Valor convertido a int: ")
+		fmt.Println(convertValue)
+	} else {
+		fmt.Println(err)
+	}
+	// A medida que se ejecuta ingreso datos
+	fmt.Print("¿Cuál es tu edad?")
+	var edad int
+	fmt.Scanln(&edad)
+	if edad >= 18 {
+		fmt.Print("Eres MAYOR de edad")
+	} else {
+		fmt.Print("Eres MENOR de edad")
+	}
 }
 
 func holaGo() {
-	fmt.Println("Hola desde GO!")
+	fmt.Println("****Mi programa con GO!****")
 }
