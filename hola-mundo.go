cdb51bf915979623076fd859520e7db9b5f64d6f
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-06-30 17:09:44 -0400

    feat: how works swotch in go

--- hola-mundo.go
+++ hola-mundo.go
@@ -1,26 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 5; i++ {
-		if i%2 == 0 {
-			fmt.Println("El número: ", i, " es par")
-		} else {
-			fmt.Println("El número: ", i, "es impar")
-		}
-	}
+	momento := time.Now().Local()
+	hoy := momento.Weekday()
 
-	peliculas := []string{"Gran Torino", "The Book's thief", "Romeo + Juliet"}
-	for i := 0; i < len(peliculas); i++ {
-		if peliculas[i] == "The Book's thief" {
-			fmt.Println("De seguro lloraste con", peliculas[i])
-		} else {
-			fmt.Println("Wenas pelis t gustan krnal", peliculas[i])
-		}
-	}
-
-	for _, pelicula := range peliculas {
-		fmt.Println("Recorriendo slice:", pelicula)
+	switch hoy {
+	case 0:
+		fmt.Println("Hoy es Lunes")
+	case 1:
+		fmt.Println("Hoy es Martes")
+	case 2:
+		fmt.Println("Hoy es Miércoles")
+	case 3:
+		fmt.Println("Hoy es Jueves")
+	case 4:
+		fmt.Println("Hoy es Viernes")
+	default:
+		fmt.Println("Hoy es otro día")
 	}
 }
