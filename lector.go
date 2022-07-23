3cf30bfbe436ef45f29b2198b5a963b1515641bc
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-07-23 09:06:55 -0400

    feat: Read and write files with go

--- lector.go
+++ lector.go
@@ -4,17 +4,35 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Lector")
-	lng, err := ioutil.ReadFile("lenguajes.txt")
-	fmt.Println("Lenguajes de programacion", lng)
-	showError(err)
+	fmt.Println("-----Lector-----")
+	writeFile()
+	readFile()
 }
 
 func showError(e error) {
 	if e != nil {
 		panic(errors.New(e.Error()))
 	}
+}
+
+func writeFile() {
+	nuevoTexto := os.Args[1]
+	// escribe := ioutil.WriteFile("lenguajes.txt", nuevoTexto, 0755)
+	fichero, err := os.OpenFile("lenguajes.txt", os.O_APPEND, 0777)
+	showError(err)
+
+	escribe, err := fichero.WriteString(string("\n" + "5. " + nuevoTexto))
+	fichero.Close()
+	showError(err)
+	fmt.Println(escribe)
+}
+
+func readFile() {
+	lng, err := ioutil.ReadFile("lenguajes.txt")
+	fmt.Println("Lenguajes de programacion", string(lng))
+	showError(err)
 }
