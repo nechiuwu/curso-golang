8061ddca9340f9267939038a8081782f7629895c
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-07-30 06:36:45 -0400

    feat: router added

--- main.go
+++ main.go
@@ -4,15 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	// crea endpoint y que hará cuando sea invocado
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "devuelve texto")
-	})
+	// URLs amigables usando /
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contacto", Contact)
 
-	// crea un server
-	server := http.ListenAndServe(":8080", nil)
+	/*
+		// crea endpoint y que hará cuando sea invocado
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "devuelve texto")
+		})
+	*/
+
+	// crea un server y recibe router
+	server := http.ListenAndServe(":8080", router)
 	log.Fatal(server)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "devuelve texto")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola, contactame")
 }
