46a3ee699f39648c32b9fc596f63e6ddbf4d15d0
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-08-04 06:31:52 -0400

    feat: added connection to mongodb

---
+++ connect_db.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func connectMongoDb() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Exito ping")
+
+	client.Database("golang")
+	if client.Database("golang").Collection("movies") == nil {
+		collection := client.Database("golang").CreateCollection(ctx, "movies")
+		log.Println(collection)
+	}
+}
--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	// URLs amigables usando /
 	router := NewRouter()
 
+	// Levanta conexion a DB
+	connectMongoDb()
+
 	// crea un server y recibe router
 	server := http.ListenAndServe(":8080", router)
 	log.Fatal(server)
